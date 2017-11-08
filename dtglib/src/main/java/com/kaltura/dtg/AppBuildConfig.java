440a383eef3fec06e559b492143449c57b0b118e
Egor Neliuba <eneliuba@gmail.com>
2017-11-08 17:48:59 +0200

    Make AppBuildConfig errors less intrusive

--- dtglib/src/main/java/com/kaltura/dtg/AppBuildConfig.java
+++ dtglib/src/main/java/com/kaltura/dtg/AppBuildConfig.java
@@ -9,16 +9,21 @@ public class AppBuildConfig {
     public static boolean DEBUG;
 
     public static Object get(Context context, String fieldName) {
+        String className = context.getPackageName() + ".BuildConfig";
         try {
-            Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
+            Class<?> clazz = Class.forName(className);
             Field field = clazz.getField(fieldName);
             return field.get(null);
+        } catch (ClassNotFoundException e) {
+            Log.e("AppBuildConfig", "Could not find class " + className + ". "
+                    + "Setting AppBuildConfig.DEBUG to false");
+            return null;
         } catch (Exception e) {
             Log.e("AppBuildConfig", "Error", e);
             return null;
         }
     }
-    
+
     public static void init(Context context) {
         Boolean debug = (Boolean) get(context, "DEBUG");
         DEBUG = debug != null ? debug : false;
