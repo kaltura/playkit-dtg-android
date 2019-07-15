6cae503e47492457e4c772d62d441dcf74ae7c3d
Noam Tamim <noam.tamim@kaltura.com>
2019-07-15 15:52:23 +0300

    Rename package com.kaltura.android.exoplayer2
    To prevent collisions with PlayKit.

--- dtglib/src/main/java/com/kaltura/dtg/BaseTrack.java
+++ dtglib/src/main/java/com/kaltura/dtg/BaseTrack.java
@@ -4,7 +4,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 
-import com.kaltura.android.exoplayer2.Format;
+import com.kaltura.android.exoparser.Format;
 import com.kaltura.dtg.dash.DashTrack;
 import com.kaltura.dtg.hls.HlsAsset;
 
