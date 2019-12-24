b5b132986109b3e7923359faca783baaae117f32
Gilad Nadav <gilad.nadav@kaltura.com>
2019-12-24 12:15:05 +0200

    rename to assertItemInput

--- dtglib/src/main/java/com/kaltura/dtg/DownloadService.java
+++ dtglib/src/main/java/com/kaltura/dtg/DownloadService.java
@@ -253,7 +253,7 @@ public class DownloadService extends Service {
         }
     }
 
-    private void assertValidItem(String itemId, String contentURL) {
+    private void assertItemInput(String itemId, String contentURL) {
 
         if (TextUtils.isEmpty(itemId) || TextUtils.isEmpty(contentURL) || Uri.parse(contentURL).getLastPathSegment() == null) {
             throw new IllegalArgumentException("item is null or contentURL is not valid contentURL = " + contentURL);
@@ -515,7 +515,7 @@ public class DownloadService extends Service {
 
     DownloadItemImp createItem(String itemId, String contentURL) throws Utils.DirectoryNotCreatableException {
         assertStarted();
-        assertValidItem(itemId, contentURL);
+        assertItemInput(itemId, contentURL);
         // if this item was just removed, unmark it as removed.
         removedItems.remove(itemId);
 
@@ -540,7 +540,7 @@ public class DownloadService extends Service {
         Utils.mkdirsOrThrow(itemDataDir);
 
         item.setDataDir(itemDataDir.getAbsolutePath());
-        
+
         database.addItemToDB(item, itemDataDir);
 
         item.setService(this);
